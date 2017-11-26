deba798503bc9cea04b256c59f222910c3623999
Piotr Słuja <psluja@gmail.com>
2017-11-26 08:36:13 +0100

    added extension to IObjectPool

---
+++ psluja.ObjectPools/ObjectPoolExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace psluja.ObjectPools
+{
+    public static class ObjectPoolExtensions
+    {
+        public static Task<T> GetObject<T>(this IObjectPool<T> pool) where T : class
+        {
+            return pool.GetObject(CancellationToken.None);
+        }
+    }
+}
